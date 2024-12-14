4ef01b67f549c439403222637297cccc75ab69b8
fmn <franciscomaderanatera@gmail.com>
2024-12-14 00:48:34 -0400

    logica de la api de login

---
+++ src/api/login/index.jsx
@@ -0,0 +1,14 @@
+import { API_URL } from "../../utils/constats"
+
+export async function LogIn (user) {
+  try {
+    const response = await fetch(API_URL + "/login", {
+      method: "post",
+      body: JSON.stringify(user)
+    })
+    const token = response.data.data.jwt
+    localStorage.setItem("jwt", token)  
+  } catch (error) {
+    console.error("Error al iniciar sesión", error)
+  }
+}
