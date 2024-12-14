240b697f5d5e3abaef1100a5fbc37b13aed3c0ff
fmn <franciscomaderanatera@gmail.com>
2024-12-13 23:03:33 -0400

    login fuente y color del boton

--- src/app/Login/index.jsx
+++ src/app/Login/index.jsx
@@ -35,26 +35,26 @@ export default function LogIn () {
           src="https://cdn-icons-png.flaticon.com/512/257/257815.png"
           style={imgStyle}
         />
-        <p>Integridad y seguridad alimentaria, garantizadas</p>
+        <p style={{ fontFamily: "DM Serif Text" }}>Integridad y seguridad alimentaria, garantizadas</p>
       </Col>
       <Col span={8}>
-        <h1 style={centerStyle}>L.I.M.S.</h1>
+        <h1 style={{ ...centerStyle, fontFamily: "DM Serif Text" }}>L.I.M.S.</h1>
         <Form
           layout="vertical"
           form={form}
           style={{ maxWidth: 600 }}
         >
-          <Form.Item label="Usuario">
+          <Form.Item label="Usuario" className="inter-font">
             <Input type="email" />
           </Form.Item>
-          <Form.Item label="Contraseña">
+          <Form.Item label="Contraseña" className="inter-font">
             <Input type="password" />
           </Form.Item>
           <Form.Item style={centerStyle}>
             <Button 
               type="primary" 
               shape="round" 
-              style={{ background: "green", borderColor: "white" }}
+              style={{ background: "#16D9CE", borderColor: "white" }}
               onClick={handleSubmit}
             >
               Log In
