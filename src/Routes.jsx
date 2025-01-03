43fdc4dfe786aad3c84ac188a35127a98001a54f
fmn <franciscomaderanatera@gmail.com>
2025-01-03 17:52:36 -0400

    arreglando el enrutado de las paginas

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,22 +1,7 @@
-import LogIn from "./pages/login/index.jsx"
-import NotFoundPage from "./pages/not-found/index.jsx"
+import LogIn from "./app/login/index.jsx";
+import NotFoundPage from "./app/not-found/index.jsx";
 
 export const Routes = [
   { path: "*", element: <NotFoundPage /> },
   { path: "/", element: <LogIn /> },
-//  {
-//    path: "app",
-//    element: <AppRouterPage />,
-//    children: [
-//      { path: "dashboard", element: <DashboardPage /> },
-//      { path: "admin/people", element: <PeoplePage />},
-//      { path: "admin/vehicles", element: <VehiclesPage />},
-//      { path: "admin/users", element: <UsersPage /> },
-//      { path: "admin/employees", element: <EmployeesPage /> },
-//      { path: "request-fuel", element: <RequestFuelView /> },
-//      { path: "assign/request", element: <DeliveryByRequestView /> },        
-//      { path: "assign/static", element: <DeliveryMonthlyView /> }, 
-//      { path: "register-tickets", element: <TicketRegistration /> },     
-//    ],
-//  },
-]
+];
