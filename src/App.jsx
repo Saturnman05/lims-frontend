8410a13d1432df813bb03e53dc6cc2910ae97a4b
fmn <franciscomaderanatera@gmail.com>
2025-01-03 18:03:58 -0400

    arreglado el enrutado

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { RouterProvider, createBrowserRouter } from "react-router";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Routes } from "./Routes"
 
 const router = createBrowserRouter(Routes)
--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,8 +1,8 @@
-import LogIn from "./pages/login/index.jsx";
-import NotFoundPage from "./pages/not-found/index.jsx";
-import HomeLabManager from "./app/HomeLabManager";
-import SampleManagement from "./app/SampleManagement";
-import SampleAssignation from "./app/SampleAssignation";
+import LogIn from "./app/login/index.jsx";
+import NotFoundPage from "./app/not-found/index.jsx";
+import HomeLabManager from "./app/HomeLabManager/index.jsx";
+import SampleAssignation from "./app/SampleAssignation/index.jsx";
+import SampleManagement from "./app/SampleManagement/index.jsx";
 
 export const Routes = [
   { path: "*", element: <NotFoundPage /> },
@@ -10,19 +10,4 @@ export const Routes = [
   { path: "homeLabManager", element: <HomeLabManager /> },
   { path: "sampleAssignation", element: <SampleAssignation /> },
   { path: "sampleManagement", element: <SampleManagement /> },
-  //  {
-  //    path: "app",
-  //    element: <AppRouterPage />,
-  //    children: [
-  //      { path: "dashboard", element: <DashboardPage /> },
-  //      { path: "admin/people", element: <PeoplePage />},
-  //      { path: "admin/vehicles", element: <VehiclesPage />},
-  //      { path: "admin/users", element: <UsersPage /> },
-  //      { path: "admin/employees", element: <EmployeesPage /> },
-  //      { path: "request-fuel", element: <RequestFuelView /> },
-  //      { path: "assign/request", element: <DeliveryByRequestView /> },
-  //      { path: "assign/static", element: <DeliveryMonthlyView /> },
-  //      { path: "register-tickets", element: <TicketRegistration /> },
-  //    ],
-  //  },
 ];
