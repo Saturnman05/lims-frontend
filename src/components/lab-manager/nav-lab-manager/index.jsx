62e25364d964a907a18957b79b9ffdbdcd22c24f
DerekSave <workderekceo@gmail.com>
2025-01-08 22:51:57 -0400

    Doing the dropdown menu - Lab manager

--- src/components/lab-manager/nav-lab-manager/index.jsx
+++ src/components/lab-manager/nav-lab-manager/index.jsx
@@ -1,10 +1,12 @@
 import { Link, useLocation } from "react-router-dom";
+import { useState } from "react";
 import PropTypes from "prop-types";
 import { UserOutlined } from "@ant-design/icons";
 import { Avatar } from "antd";
 import "../../../styles/lab-manager/nav-lab-manager/index.css";
 
 export function NavLabManager() {
+  const [open, setOpen] = useState(false);
   return (
     <nav className="nav-lab-header">
       <div className="navbar-logo">
@@ -36,13 +38,31 @@ export function NavLabManager() {
           <CustomLink to="/layout-lab-manager/support">Soporte</CustomLink>
         </li>
         <li>
-          <CustomLink to="/layout-lab-manager/account">
-            <Avatar
-              className="custom-avatar"
-              size={30}
-              icon={<UserOutlined />}
-            />
-          </CustomLink>
+          <Avatar
+            className="custom-avatar"
+            size={30}
+            icon={<UserOutlined />}
+            onClick={() => setOpen(!open)}
+          />
+          {open && (
+            <ul className="dropdown-menu">
+              <li>
+                <CustomLink to="/layout-lab-manager/management/users">
+                  Usuarios
+                </CustomLink>
+              </li>
+              <li>
+                <CustomLink to="/layout-lab-manager/management/roles">
+                  Roles
+                </CustomLink>
+              </li>
+              <li>
+                <CustomLink to="/layout-lab-manager/management/settings">
+                  Configuración
+                </CustomLink>
+              </li>
+            </ul>
+          )}
         </li>
       </ul>
     </nav>
@@ -61,6 +81,22 @@ function CustomLink({ to, children, ...props }) {
   );
 }
 
+function DropdownMenu() {
+  function DropdownItem(props) {
+    <CustomLink to={props.to} className="menu-item">
+      <span className="icon-button">{props.leftIcon}</span>
+      {props.children}
+      <span className="icon-right">{props.rightIcon}</span>
+    </CustomLink>;
+  }
+  return (
+    <div className="dropdown">
+      <DropdownItem>A</DropdownItem>
+      <DropdownItem>B</DropdownItem>
+      <DropdownItem>C</DropdownItem>
+    </div>
+  );
+}
 CustomLink.propTypes = {
   to: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
