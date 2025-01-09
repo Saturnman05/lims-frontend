baa7c7ed8d42f38ac28c4df58e5d00260efc6fec
fmn <franciscomaderanatera@gmail.com>
2025-01-09 10:11:28 -0400

    cambiar la navegacion del login

--- src/hooks/login.js
+++ src/hooks/login.js
@@ -25,7 +25,7 @@ export function useLogin() {
     console.log("en el onfinish:", values);
     try {
       await mockLogIn(values);
-      navigate("/layout/homeLabManager");
+      navigate("/layout-lab-manager/home-lab-manager");
     } catch (error) {
       console.error(error);
     }
