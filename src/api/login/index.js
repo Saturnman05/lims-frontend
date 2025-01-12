fa0161ffc59950e9976de6dbe124b87f4b251a8b
fmn <franciscomaderanatera@gmail.com>
2025-01-12 19:17:08 -0400

    validar correctamente usuario y contraseña en el login

--- src/api/login/index.js
+++ src/api/login/index.js
@@ -1,4 +1,5 @@
 import { API_URL } from "../../utils/constats"
+import { message } from "antd";
 
 export async function logIn (user) {
   const response = await fetch(`${API_URL}token/`, {
@@ -8,7 +9,13 @@ export async function logIn (user) {
     },
     body: JSON.stringify(user),
   });
-  const data = await response.json();
-  const token = data.access;
-  localStorage.setItem("jwt", token);
+
+  if (response.ok) {
+    const data = await response.json();
+    const token = data.access;
+    localStorage.setItem("jwt", token);
+  } else {
+    message.error("Error. Usuario o contraseña incorrecta");
+    throw new Error("Error. Nombre de usuario o contraseña incorrecta.")
+  }
 }
