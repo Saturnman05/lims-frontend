beabb7d1f4561b2382d9b4b53600cf7450bb127e
fmn <franciscomaderanatera@gmail.com>
2025-01-16 17:40:43 -0400

    integracion con los endpoints de categoria y alergenos

--- src/api/samples/index.js
+++ src/api/samples/index.js
@@ -1,9 +1,17 @@
+import { API_URL } from "../../utils/constats";
+
 export async function getCategorysOptions() {
-  return [
-    { value: "uno", label: "uno" },
-    { value: "dos", label: "dos" },
-    { value: "tres", label: "tres" },
-  ]
+  const token = localStorage.getItem("jwt");
+  const response = await fetch(`${API_URL}categorys/`, {
+    method: "get",
+    headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`,
+    },
+  });
+  const data = await response.json();
+  const categorys = data.map(category => ({value: category.category_name, label: category.category_name}));
+  return categorys;
 }
 
 export async function getSubCategorysOptions() {
@@ -15,9 +23,15 @@ export async function getSubCategorysOptions() {
 }
 
 export async function getAllergensOptions() {
-  return [
-    { value: "uno", label: "uno" },
-    { value: "dos", label: "dos" },
-    { value: "tres", label: "tres" },
-  ]
+  const token = localStorage.getItem("jwt");
+  const response = await fetch(`${API_URL}allergens/`, {
+    method: "get",
+    headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`,
+    },
+  });
+  const data = await response.json();
+  const allergens = data.map(allergen => ({value: allergen.allergen_name, label: allergen.allergen_name}));
+  return allergens;
 }
